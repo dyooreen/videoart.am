e75ac92633fa7b5d79ba68d5dad275d0287f579a
dyooreen <dyooreen@icloud.com>
2023-11-13 23:26:10 +0400

    .

--- app/next/lib/host.tsx
+++ app/next/lib/host.tsx
@@ -3,10 +3,20 @@ const getHost = () => {
 
   return process.env.STAGE == "production" ? h[0] : h[1];
 };
-const fullPath = (p: string, isImage: boolean = false): string => {
+
+const fullPath = (
+  p: string,
+  isOnServerSide: boolean = false,
+  isImage: boolean = false
+): string => {
+  if (isOnServerSide && isImage) {
+    return "http://strapi:1337" + p;
+  }
+
   if (isImage) {
     return "https://videoart.am/api" + p;
   }
+
   return getHost() + p;
 };
 export default fullPath;
